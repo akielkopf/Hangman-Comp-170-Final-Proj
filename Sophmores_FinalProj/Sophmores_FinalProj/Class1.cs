e090ed86949ab2f32d374c4a3a121aa9ea1f642b
Rhernandez513 <rhernandez513@gmail.com>
2015-04-01 15:30:45 -0500

    utilities

---
+++ Sophmores_FinalProj/Sophmores_FinalProj/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sophmores_FinalProj
+{
+    public class Item
+    {
+      private string name;
+      private string flavortext;
+      private int physicalDamage;
+      private int magicDamage;
+
+    }
+}
