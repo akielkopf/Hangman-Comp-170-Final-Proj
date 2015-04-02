023c3d02fbfaa4b7223159996d07fe748357a53d
Rhernandez513 <rhernandez513@gmail.com>
2015-04-02 17:34:51 -0500

    Base Done, ItemAdd still to do

---
+++ Sophmores_FinalProj/Sophmores_FinalProj/Inventory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sophmores_FinalProj
+{
+  public class Inventory
+  {
+    private Dictionary<string, int> contents;
+    public Inventory()
+    {
+      contents = new Dictionary<string, int>();
+    }
+    public Inventory (Inventory OldInventory)
+    {
+      contents = new Dictionary<string,int>(OldInventory.contents);
+    }
+    public void Add(string ItemName, int ItemCount)
+    {
+      if (contents.ContainsKey(ItemName))
+      {
+        contents[ItemName] += ItemCount;
+      }
+      else()
+        ///////////// continue here
+    //        void AddToInventory()
+    //{
+
+    //}
+    //void RemoveFromInventory()
+    //{
+
+    //}
+    }
+  }
+}
