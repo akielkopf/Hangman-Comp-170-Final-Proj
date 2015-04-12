18b25eef9abcbea046b05a1b288164ae685e1aaa
Rhernandez513 <rhernandez513@gmail.com>
2015-04-11 22:46:30 -0500

    Implemented a Key Item type

---
+++ Sophmores_FinalProj/Sophmores_FinalProj/Key.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sophmores_FinalProj
+{
+  class Key : Item
+  {
+    /// <summary>
+    /// Keys, type Key, are consumable
+    /// </summary>
+    public Key()
+    {
+      type = "Key";
+      consumable = true;
+    }
+  }
+}
