c6a5ba208174a2d16c34f670ceb12d163e0e519f
Rhernandez513 <rhernandez513@gmail.com>
2015-04-26 02:48:33 -0500

    forgot to stage merge conflict results

--- Sophmores_FinalProj/Sophmores_FinalProj/Utilities/TextUtil.cs
+++ Sophmores_FinalProj/Sophmores_FinalProj/Utilities/TextUtil.cs
@@ -1,36 +1,20 @@
-<<<<<<< HEAD
-﻿using IntroCS;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-=======
-﻿using System;
 using System.IO;
 using System.Text;
->>>>>>> textUtiltweaks
 
 namespace Sophmores_FinalProj.Utilities
 {
   /// <summary>
-  /// Holds Utility code for manipulating the console window 
+  /// Holds Utility code for manipulating the console window
   /// </summary>
   public class TextUtil
   {
-<<<<<<< HEAD
-    #region Public Methods
-
-    /// <summary>
-    /// Prints standard Press Any Key to Continue Message Then clears the screen 
-=======
     private static string fullFilePath = string.Empty;
     private static char[] noChar = { };
 
     /// <summary>
     /// Prints standard Press Any Key to Continue Message Then clears
     /// the screen
->>>>>>> textUtiltweaks
     /// </summary>
     public static void PressAnyKeyBufferClear()
     {
@@ -41,12 +25,8 @@ namespace Sophmores_FinalProj.Utilities
     }
 
     /// <summary>
-<<<<<<< HEAD
-    /// Prints custom Press Any Key to Continue message Then clears the screen 
-=======
     /// Prints custom Press Any Key to Continue message Then clears
     /// the screen
->>>>>>> textUtiltweaks
     /// </summary>
     /// <param name="message"> Message to display to player </param>
     public static void PressAnyKeyBufferClear(string message)
@@ -57,134 +37,7 @@ namespace Sophmores_FinalProj.Utilities
     }
 
     /// <summary>
-<<<<<<< HEAD
-    /// Prints to Console the contents of the supplied text File AND Returns
-    /// file contents as String
-    /// * Throws NotSupportedException* if param is not a '.txt'
-=======
-    /// Sets Buffer Size (within the console that is in the output) 
->>>>>>> textUtiltweaks
-    /// </summary>
-    /// <param name="textFileToRead">
-    /// The text file to be read, printed, and returned
-    /// </param>
-    /// <returns> Contents of the text file as String </returns>
-    public static string PrintAndReturnTextFile(string textFileToRead)
-    {
-      string txt = ".txt";
-      if (textFileToRead.EndsWith(txt))
-      {
-        StringBuilder builder = new StringBuilder();
-        var reader = FIO.OpenReader(FIO.GetLocation(textFileToRead),
-          textFileToRead);
-        while (!(reader.EndOfStream))
-        {
-          builder.Append("\n" + reader.ReadLine());
-        }
-        string text = ("" + builder);
-        Console.WriteLine(text);
-        reader.Close();
-        return text;
-      }
-      else
-      {
-<<<<<<< HEAD
-        string msg = "That is not a text file!";
-        Console.WriteLine(msg);
-        throw new NotSupportedException(msg);
-=======
-        Console.BufferHeight = (Int16.MaxValue - 1);
-        Console.BufferWidth = (80);
->>>>>>> textUtiltweaks
-      }
-    }
-
-    /// <summary>
-<<<<<<< HEAD
-    /// Prints to Console contents of the supplied text file
-    /// * Throws NotSupportedException* if param is not a '.txt'
-    /// </summary>
-    /// <param name="textFileToRead"> The text file to be read and printed </param>
-    public static void PrintTextFile(string textFileToRead)
-=======
-    /// Checks user OS platform 
-    /// </summary>
-    /// <returns> True if Windows, false otherwise </returns>
-    private static bool CheckOS()
->>>>>>> textUtiltweaks
-    {
-      string txt = ".txt";
-      if (textFileToRead.EndsWith(txt))
-      {
-        StringBuilder builder = new StringBuilder();
-        var reader = FIO.OpenReader(FIO.GetLocation(textFileToRead),
-          textFileToRead);
-        while (!(reader.EndOfStream))
-        {
-          builder.Append("\n" + reader.ReadLine());
-        }
-        string text = ("" + builder);
-        Console.WriteLine(text);
-        reader.Close();
-      }
-      else
-      {
-        string msg = "That is not a text file!";
-        Console.WriteLine(msg);
-        throw new NotSupportedException(msg);
-      }
-    }
-
-    /// <summary>
-<<<<<<< HEAD
-    /// Returns the text file. 
-=======
-    /// Prints to Console contents of the supplied text file 
->>>>>>> textUtiltweaks
-    /// </summary>
-    /// <returns> The text file. </returns>
-    /// <param name="textFileToRead"> Text file to read. </param>
-    public static string ReturnTextFile(string textFileToRead)
-    {
-      string txt = ".txt";
-      if (textFileToRead.EndsWith(txt))
-      {
-<<<<<<< HEAD
-        StringBuilder builder = new StringBuilder();
-        var reader = FIO.OpenReader(FIO.GetLocation(textFileToRead),
-                      textFileToRead);
-        while (!(reader.EndOfStream))
-        {
-          builder.Append("\n" + reader.ReadLine());
-        }
-        string text = ("" + builder);
-        reader.Close();
-        return text;
-=======
-        string Location = FindTextFile(textFileToRead);
-        if (Location != null)
-        {
-          Console.WriteLine(TextFileContentsFromFullPath(Location));
-        }
-        else
-        {
-          string msg = "File not found!";
-          Console.WriteLine(msg);
-          throw new FileNotFoundException(msg);
-        }
->>>>>>> textUtiltweaks
-      }
-      else
-      {
-        string msg = "That is not a text file!";
-        Console.WriteLine(msg);
-        throw new NotSupportedException(msg);
-      }
-    }
-<<<<<<< HEAD
-
-    /// <summary>
-    /// Sets Buffer Size (within the console that is in the output) 
+    /// Sets Buffer Size (within the console that is in the output)
     /// </summary>
     public static void SetBufferSize()
     {
@@ -195,12 +48,8 @@ namespace Sophmores_FinalProj.Utilities
       }
     }
 
-    #endregion Public Methods
-
-    #region Private Methods
-
     /// <summary>
-    /// Checks user OS platform 
+    /// Checks user OS platform
     /// </summary>
     /// <returns> True if Windows, false otherwise </returns>
     private static bool CheckOS()
@@ -214,8 +63,36 @@ namespace Sophmores_FinalProj.Utilities
       return false;
     }
 
-    #endregion Private Methods
-=======
+    /// <summary>
+    /// Prints to Console contents of the supplied text file
+    /// </summary>
+    /// <param name="textFileToRead">
+    /// The text file to be read and printed
+    /// </param>
+    public static void PrintTextFile(string textFileToRead)
+    {
+      string txt = ".txt";
+      if (textFileToRead.EndsWith(txt))
+      {
+        string Location = FindTextFile(textFileToRead);
+        if (Location != null)
+        {
+          Console.WriteLine(TextFileContentsFromFullPath(Location));
+        }
+        else
+        {
+          string msg = "File not found!";
+          Console.WriteLine(msg);
+          throw new FileNotFoundException(msg);
+        }
+      }
+      else
+      {
+        string msg = "That is not a text file!";
+        Console.WriteLine(msg);
+        throw new NotSupportedException(msg);
+      }
+    }
 
     /// <summary>
     /// Returns the contents of a text file verbatim
@@ -262,7 +139,7 @@ namespace Sophmores_FinalProj.Utilities
     }
 
     /// <summary>
-    /// Returns the CONTENTS of the text file. 
+    /// Returns the CONTENTS of the text file.
     /// </summary>
     /// <returns> The text file. </returns>
     /// <param name="textFileToRead"> Text file to read. </param>
@@ -335,13 +212,13 @@ namespace Sophmores_FinalProj.Utilities
         {
           throw new DirectoryNotFoundException("The directory does not exist.");
         }
-        // Call the GetFileSystemInfos method. 
+        // Call the GetFileSystemInfos method.
         FileSystemInfo[] infos = dir.GetFileSystemInfos();
-        // Pass the result to the ListDirectoriesAndFiles method 
+        // Pass the result to the ListDirectoriesAndFiles method
         ListDirectoriesAndFiles(infos, textFile);
         if (fullFilePath.EndsWith(textFile))
         {
-          // Returns full filepath if found 
+          // Returns full filepath if found
           return fullFilePath;
         }
       }
@@ -349,36 +226,36 @@ namespace Sophmores_FinalProj.Utilities
     }
 
     /// <summary>
-    /// Helper function for FindTextFile 
+    /// Helper function for FindTextFile
     /// </summary>
     /// <returns> Full Path to File, or null if file not found </returns>
     private static void ListDirectoriesAndFiles(FileSystemInfo[] FSInfo, string File)
     {
-      // Helps back out of recursion 
+      // Helps back out of recursion
       if (fullFilePath.EndsWith(File))
       {
         return;
       }
-      // Check the FSInfo parameter. 
+      // Check the FSInfo parameter.
       if (FSInfo == null)
       {
         throw new ArgumentNullException("FSInfo");
       }
-      // Iterate through each item. 
+      // Iterate through each item.
       foreach (FileSystemInfo i in FSInfo)
       {
-        // Check to see if this is a DirectoryInfo object. 
+        // Check to see if this is a DirectoryInfo object.
         if (i is DirectoryInfo)
         {
-          // Cast the object to a DirectoryInfo object. 
+          // Cast the object to a DirectoryInfo object.
           DirectoryInfo dInfo = (DirectoryInfo)i;
-          // Iterate through all sub-directories. 
+          // Iterate through all sub-directories.
           ListDirectoriesAndFiles(dInfo.GetFileSystemInfos(), File);
         }
-        // Check to see if this is a FileInfo object. 
+        // Check to see if this is a FileInfo object.
         else if (i is FileInfo)
         {
-          // Checks against passed in file 
+          // Checks against passed in file
           if (i.FullName.EndsWith(File))
           {
             fullFilePath = i.FullName;
@@ -387,6 +264,5 @@ namespace Sophmores_FinalProj.Utilities
         }
       }
     }
->>>>>>> textUtiltweaks
   }
 }
