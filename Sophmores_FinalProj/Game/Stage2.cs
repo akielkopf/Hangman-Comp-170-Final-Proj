b0dac9fa513a9535dc715a5d9dce9e754e3ed9cd
Rhernandez513 <rhernandez513@gmail.com>
2015-05-01 02:34:05 -0500

    broke up narration text into files and start of stage2.cs

---
+++ Sophmores_FinalProj/Game/Stage2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sophmores_FinalProj.Utilities;
+
+namespace Sophmores_FinalProj
+{
+  public static class Stage2
+  {
+    public static void PrintMsgs()
+    {
+      string[] stageTexts = GetNarrationFiles();
+    }
+
+    private static string[] GetNarrationFiles()
+    {
+      List<string> stageTexts = new List<string>();
+      for (int i = 0; i < 22; i++)
+      {
+        string file = ("Stage2_" + i + ".txt");
+        if (i == 15)
+        {
+          string jumpFile = ("Jump_Option_");
+          file = jumpFile + file;
+        }
+        else if (i == 16)
+        {
+          string climbFile = ("ClimbDown_Option_");
+          file = climbFile + file;
+        }
+        stageTexts.Add(file);
+      }
+      return stageTexts.ToArray();
+    }
+  }
+}
